d5283f6500d4f3adc6d83d362002ec1a3775c954
phuang00 <phuang00@stuy.edu>
2019-10-26 21:17:38 -0400

    added more test cases

--- main.c
+++ main.c
@@ -9,7 +9,9 @@ int main() {
   printf("Adding #s 0-9 to list.\n");
   int i = 0;
   for (i = 0; i < 10; i++){
+    printf("Adding node: %d\n", i);
     list = insert_front(list, i);
+    print_list(list);
   }
 
   printf("Printing list:\n");
@@ -21,20 +23,34 @@ int main() {
   printf("Printing list:\n");
   print_list(list);
 
-  printf("Adding #s 0-9 to list.\n");
-  for (i = 0; i < 10; i++){
+  printf("Adding #s 10-19 to list.\n");
+  for (i = 10; i < 20; i++){
     list = insert_front(list, i);
   }
 
   printf("Printing list:\n");
   print_list(list);
 
-  printf("Removing #s 0-20 from list.\n");
-  for (i = 0; i <= 20; i++){
+  printf("Removing # 10 from list.\n");
+  list = remove_node(list, 10);
+  print_list(list);
+
+  printf("Removing # 19 from list.\n");
+  list = remove_node(list, 19);
+  print_list(list);
+
+  printf("Removing #12-17 from list.\n");
+  for (i = 12; i <= 17; i++){
     printf("Removing %d\n", i);
-    print_list(list);
     list = remove_node(list, i);
+    print_list(list);
   }
+
+  printf("Printing list:\n");
+  print_list(list);
+
+  printf("Freeing the remainder of the list.\n");
+  list = free_list(list);
 
   printf("Printing list:\n");
   print_list(list);
