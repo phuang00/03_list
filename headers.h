38647e1b65d8e0183d2f280daf817fe3389c85f1
phuang00 <phuang00@stuy.edu>
2019-10-26 21:20:24 -0400

    fixed headers

--- headers.h
+++ headers.h
@@ -3,10 +3,10 @@
 
 struct node { int i; struct node *next; };
 
-void print_list(struct node *);
+void print_list(struct node *n);
 
-struct node * insert_front(struct node *, int);
+struct node * insert_front(struct node *n, int x);
 
-struct node * free_list(struct node *);
+struct node * free_list(struct node *n);
 
 struct node * remove_node(struct node *front, int data);
